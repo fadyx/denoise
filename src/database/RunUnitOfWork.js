80d382f640e6f975d20c944feb3d04db79d3532c
f <>
2021-10-16 01:57:40 +0200

    linting and fixing unknown words

--- src/database/RunUnitOfWork.js
+++ src/database/RunUnitOfWork.js
@@ -1,30 +1,30 @@
 import mongoose from "mongoose";
 
-class UnitOfWork {
-	constructor(uow) {
-		this.uow = uow;
-	}
+// class UnitOfWork {
+// 	constructor(uow) {
+// 		this.uow = uow;
+// 	}
 
-	async run(session) {
-		if (typeof session === "undefined" || session === null) {
-			this.session = await mongoose.startSession();
-			this.session.startTransaction();
-		} else {
-			this.session = session;
-		}
+// 	async run(session) {
+// 		if (typeof session === "undefined" || session === null) {
+// 			this.session = await mongoose.startSession();
+// 			this.session.startTransaction();
+// 		} else {
+// 			this.session = session;
+// 		}
 
-		try {
-			const result = await this.uow(session);
-			await session.commitTransaction();
-			return result;
-		} catch (error) {
-			await session.abortTransaction();
-			throw error;
-		} finally {
-			await session.endSession();
-		}
-	}
-}
+// 		try {
+// 			const result = await this.uow(session);
+// 			await session.commitTransaction();
+// 			return result;
+// 		} catch (error) {
+// 			await session.abortTransaction();
+// 			throw error;
+// 		} finally {
+// 			await session.endSession();
+// 		}
+// 	}
+// }
 
 const RunUnitOfWork = async (uow, passedSession) => {
 	let session;
